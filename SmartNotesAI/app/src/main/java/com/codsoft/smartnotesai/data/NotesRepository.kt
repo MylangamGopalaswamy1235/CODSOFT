b80387a364cbcdd2fbdd1b2ce7f370549670fa38
MylangamGopalaswamy1235 <mylangambalajigopalaswamy1235@gmail.com>
2026-04-01 02:01:06 +0530

    Delete SmartNotesAI directory

--- SmartNotesAI/app/src/main/java/com/codsoft/smartnotesai/data/NotesRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.codsoft.smartnotesai.data
-
-class NotesRepository(private val noteDao: NoteDao) {
-    val notes = noteDao.getAllNotes()
-
-    suspend fun addNote(note: Note) = noteDao.insert(note)
-}
--- SmartNotesAI/app/src/main/java/com/codsoft/smartnotesai/data/TodoRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.codsoft.smartnotesai.data
-
-class TodoRepository(private val todoDao: TodoDao) {
-    val tasks = todoDao.getAllTasks()
-
-    suspend fun addTask(task: TodoTask) = todoDao.insert(task)
-    suspend fun updateTask(task: TodoTask) = todoDao.update(task)
-}
